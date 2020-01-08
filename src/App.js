6e1bccd182aafcb58e9da2a6c0f550ec1736649f
pumpkinzomb <pumpkinzomb@gmail.com>
2020-01-08 21:41:12 +0900

    loading 추가

--- src/App.js
+++ src/App.js
@@ -4,56 +4,78 @@ import Tesseract from "tesseract.js";
 import Clipboard from "clipboard";
 import Select from "react-select";
 import languages from "./Tesseract/language.js";
+import update from "react-addons-update";
+import ReactLoading from "react-loading";
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.textArea = React.createRef();
     this.textCopyBtn = React.createRef();
-    this.state = {
-      text: "",
-      image: "",
-      selectedLanguage: languages[1]
-    };
     this.handleChange = this.handleChange.bind(this);
     this.handleChangeSelect = this.handleChangeSelect.bind(this);
     this.readImage = this.readImage.bind(this);
     this.handleInputText = this.handleInputText.bind(this);
+    this.state = {
+      text: "",
+      image: "",
+      isLoading: false,
+      selectedLanguage: languages[1]
+    };
   }
   async readImage(img, lang) {
     const {
       data: { text }
     } = await Tesseract.recognize(img, lang);
     console.log("read success");
-    console.log(text);
-    this.setState({
-      text,
-      selectedLanguage: this.selectedLanguage,
-      image: img
+    const updateState = update(this.state, {
+      text: { $set: text },
+      isLoading: { $set: false }
     });
+    this.setState(updateState);
   }
   handleInputText(e) {
-    this.setState({
-      text: e.target.value
+    const updateState = update(this.state, {
+      text: { $set: e.target.value }
     });
+    this.setState(updateState);
   }
   handleChange(e) {
     const image = e.target.files[0];
     if (image) {
       const imgUrl = URL.createObjectURL(image);
+      const updateState = update(this.state, {
+        image: { $set: imgUrl },
+        isLoading: { $set: true }
+      });
+      this.setState(updateState);
       this.readImage(imgUrl);
+    } else {
+      const updateState = update(this.state, {
+        image: { $set: "" }
+      });
+      this.setState(updateState);
     }
   }
   handleChangeSelect(selected, event) {
-    console.log(selected, event);
-    this.setState({
-      text: this.state.text,
-      image: this.state.image,
-      selectedLanguage: selected
-    });
     if (!this.state.image || this.state.selectedLanguage === selected) {
-      return;
+      const updateState = update(this.state, {
+        selectedLanguage: {
+          $set: selected
+        }
+      });
+      this.setState(updateState);
+    } else {
+      const updateState = update(this.state, {
+        selectedLanguage: {
+          $set: selected
+        },
+        isLoading: {
+          $set: true
+        }
+      });
+      this.setState(updateState);
+      this.readImage(this.state.image, selected.value);
     }
-    this.readImage(this.state.image, selected.value);
   }
   componentDidMount() {
     const button = this.textCopyBtn.current;
@@ -76,7 +98,7 @@ export default class App extends Component {
     this.clipboard.destroy();
   }
   render() {
-    const { text, image, selectedLanguage } = this.state;
+    const { text, image, selectedLanguage, isLoading } = this.state;
     return (
       <div className="App">
         <h1>Text Converter</h1>
@@ -89,14 +111,33 @@ export default class App extends Component {
           autoFocus={true}
         />
         <div className="camera-section">
-          <input type="file" id="camera" name="camera" capture="camera" accept="image/*" onChange={this.handleChange} />
+          <input
+            type="file"
+            id="camera"
+            name="camera"
+            capture="camera"
+            accept="image/*"
+            onChange={this.handleChange}
+          />
         </div>
         <div className="text-section">
           <div className="image">
+            {isLoading ? (
+              <div className="loading">
+                <ReactLoading type="bars" color="#000000" />
+              </div>
+            ) : (
+              ""
+            )}
             <img src={image} crossOrigin="anonymous" alt={image}></img>
           </div>
           <div className="text">
-            <textarea placeholder="" value={text} ref={this.textArea} onChange={this.handleInputText}></textarea>
+            <textarea
+              placeholder=""
+              value={text}
+              ref={this.textArea}
+              onChange={this.handleInputText}
+            ></textarea>
           </div>
           <button type="button" ref={this.textCopyBtn}>
             복사하기
