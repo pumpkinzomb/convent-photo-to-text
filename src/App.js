48bd6f7be799bf7b357d9a55d65fc553d94f9aec
pumpkinzomb <pumpkinzomb@neofect.com>
2020-01-08 17:20:41 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.scss";
+import Tesseract from "tesseract.js";
+import Clipboard from "clipboard";
+import Select from "react-select";
+import languages from "./Tesseract/language.js";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.textArea = React.createRef();
+    this.textCopyBtn = React.createRef();
+    this.state = {
+      text: "",
+      image: "",
+      selectedLanguage: languages[1]
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeSelect = this.handleChangeSelect.bind(this);
+    this.readImage = this.readImage.bind(this);
+    this.handleInputText = this.handleInputText.bind(this);
+  }
+  async readImage(img, lang) {
+    const {
+      data: { text }
+    } = await Tesseract.recognize(img, lang);
+    console.log("read success");
+    console.log(text);
+    this.setState({
+      text,
+      selectedLanguage: this.selectedLanguage,
+      image: img
+    });
+  }
+  handleInputText(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+  handleChange(e) {
+    const image = e.target.files[0];
+    if (image) {
+      const imgUrl = URL.createObjectURL(image);
+      this.readImage(imgUrl);
+    }
+  }
+  handleChangeSelect(selected, event) {
+    console.log(selected, event);
+    this.setState({
+      text: this.state.text,
+      image: this.state.image,
+      selectedLanguage: selected
+    });
+    if (!this.state.image || this.state.selectedLanguage === selected) {
+      return;
+    }
+    this.readImage(this.state.image, selected.value);
+  }
+  componentDidMount() {
+    const button = this.textCopyBtn.current;
+    const input = this.textArea.current;
+    this.clipboard = new Clipboard(button, {
+      target: () => input
+    });
+    this.clipboard.on("success", function(e) {
+      // console.info("Action:", e.action);
+      // console.info("Text:", e.text);
+      // console.info("Trigger:", e.trigger);
+      e.clearSelection();
+    });
+    this.clipboard.on("error", function(e) {
+      // console.error('Action:', e.action);
+      // console.error('Trigger:', e.trigger);
+    });
+  }
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
+  render() {
+    const { text, image, selectedLanguage } = this.state;
+    return (
+      <div className="App">
+        <h1>Text Converter</h1>
+        <Select
+          classNamePrefix="select"
+          options={languages}
+          value={selectedLanguage}
+          isSearchable={false}
+          onChange={this.handleChangeSelect}
+          autoFocus={true}
+        />
+        <div className="camera-section">
+          <input type="file" id="camera" name="camera" capture="camera" accept="image/*" onChange={this.handleChange} />
+        </div>
+        <div className="text-section">
+          <div className="image">
+            <img src={image} crossOrigin="anonymous" alt={image}></img>
+          </div>
+          <div className="text">
+            <textarea placeholder="" value={text} ref={this.textArea} onChange={this.handleInputText}></textarea>
+          </div>
+          <button type="button" ref={this.textCopyBtn}>
+            복사하기
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Tesseract/language.js
@@ -0,0 +1,11 @@
+const supportList = [
+  { value: "chi_sim", label: "Chinese - Simplified" },
+  { value: "eng", label: "English" },
+  { value: "fra", label: "French" },
+  { value: "deu", label: "German" },
+  { value: "jpn", label: "Japanese" },
+  { value: "kor", label: "Korean" },
+  { value: "nor", label: "Norwegian" }
+];
+
+export default supportList;
