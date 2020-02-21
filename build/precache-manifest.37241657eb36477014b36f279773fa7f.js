eeeb0176ec09e17efebb601c37e34ce2e49ed9e8
pumpkinzomb <pumpkinzomb@gmail.com>
2020-02-21 21:30:57 +0900

    edit body css

--- build/precache-manifest.37241657eb36477014b36f279773fa7f.js
+++ build/precache-manifest.37241657eb36477014b36f279773fa7f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d4d5d27764a1ee4584825b82613ee9cb",
+    "revision": "d2b3b7088b999253e7ed35996771f618",
     "url": "/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.117ce29f.chunk.css"
   },
   {
-    "revision": "a0d50b854e06582ced1b",
-    "url": "/static/css/main.b6638c11.chunk.css"
+    "revision": "e4ae2dbc819b54c783fb",
+    "url": "/static/css/main.d9a202ea.chunk.css"
   },
   {
     "revision": "5438c8727bce1f9f9146",
     "url": "/static/js/2.8e070718.chunk.js"
   },
   {
-    "revision": "a0d50b854e06582ced1b",
+    "revision": "e4ae2dbc819b54c783fb",
     "url": "/static/js/main.552872ac.chunk.js"
   },
   {
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86f992f29f0464bb5ae7f3c67f3465d6.js"
+  "/precache-manifest.37241657eb36477014b36f279773fa7f.js"
 );
 
 self.addEventListener('message', (event) => {
